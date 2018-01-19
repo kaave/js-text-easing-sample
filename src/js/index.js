146ef53100e8dde96d91d6e8d3129bd47b33d76e
kaave <abe@framelunch.jp>
2018-01-19 17:38:27 +0900

    とりあえず

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
 /* eslint import/first: off */
 import './common/initializer';
 import { format } from 'date-fns';
-import BezierEasing from 'bezier-easing';
+import createEasing from 'bezier-easing';
+
+const text = '吾輩は猫である名前はまだ無い';
+const easing = createEasing(0.19, 1, 0.22, 1);
+const intervalMsec = 3000;
+const totalEasingMsec = intervalMsec / 2;
+const textPositions = text.split('').map((_, i) => totalEasingMsec * easing(i / text.length));
+// console.log(textPositions);
+console.log(text.split('').map((_, i) => [i, easing(i / text.length)]));
+
+function wait(msec) {
+  return new Promise(resolve => setTimeout(() => resolve(), msec));
+}
 
 class Main {
   constructor() {
@@ -9,14 +21,26 @@ class Main {
   }
 
   onDOMContentLoaded() {
-    console.log(`DOMContentLoaded: ${format(new Date())}`);
+    const elements = Array.from(document.getElementsByClassName('-js-char-panel'));
+
+    setInterval(() => {
+      const changeChar = async (str, index) => {
+        elements.forEach((element) => { element.innerHTML = str[index]; });
+        const strLength = str.length;
+        const nextIndex = index + 1;
+        if (nextIndex < strLength) {
+          await wait(textPositions[nextIndex] - textPositions[index]);
+          changeChar(str, nextIndex);
+        }
+      };
+
+      changeChar(text, 0);
+      // for (let i = 0; i < 100; i += 1) {
+      //   console.log(easing(i / 100));
+      // }
+    }, intervalMsec);
   }
 }
 
 const main = new Main();
 window.addEventListener('DOMContentLoaded', main.onDOMContentLoaded);
-
-const easing = BezierEasing(0.19, 1, 0.22, 1);
-for (let i = 0; i < 100; i += 1) {
-  console.log(easing(i / 100));
-}
